31eeb69ee8103caff721541544b9e2f0f57ab8a8
Unknown <mathias.s.larsen99@gmail.com>
2018-02-07 14:38:43 +0100

    h

--- src/galgeleg/BenytGalgelogik.java
+++ src/galgeleg/BenytGalgelogik.java
@@ -1,56 +1,82 @@
 package galgeleg;
 
+import javax.xml.namespace.QName;
+import javax.xml.ws.Service;
+import java.net.URL;
 import java.rmi.Naming;
 import java.util.Scanner;
 
 public class BenytGalgelogik {
 
-  public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception {
 
-      GalgeI k =(GalgeI) Naming.lookup("rmi://localhost:1099/Galge");
-      Scanner scanner = new Scanner(System.in);
-      
-      System.out.println("//**\\\\**//**\\\\**//**\\\\**//**\\\\**//**\\\\**//**\\\\**//**\\\\");
-      System.out.println("//                                                  \\\\");
-      System.out.println("//    Velkommen til det fantastiske galgelej spil   \\\\");
-      System.out.println("//                                                  \\\\");
-      System.out.println("//              Tast 'y' for at starte              \\\\");
-      System.out.println("//                                                  \\\\");
-      System.out.println("//**\\\\**//**\\\\**//**\\\\**//**\\\\**//**\\\\**//**\\\\**//**\\\\");
-      
-      if (scanner.nextLine().toLowerCase().startsWith("n")) {
-      k.nulstil();
+        GalgeI k = (GalgeI) Naming.lookup("rmi://localhost:1099/Galge");
 
-      try {
-        k.hentOrdFraDr();
-      } catch (Exception e) {
-      e.printStackTrace();
-      }
-    k.logStatus();
-      }
-      System.out.println("Get ordet!!!");
-      
-      while(!k.erSpilletSlut()){
-          System.out.println("Du har gættet forkert " +k.getAntalForkerteBogstaver()+ " gange.");
-          System.out.println("Hvis du gætter forkert 7 gange har du tabt :)");
-          System.out.println("");
-          System.out.println("Gæt på et bogstav!!!");
-          System.out.println(k.getSynligtOrd());
-          System.out.println("");
-          System.out.println("Du har gættet på fælgende bogstaver " +k.getBrugteBogstaver());
-         
-          k.gætBogstav(scanner.nextLine().toLowerCase());
-          k.logStatus();
-             
-      }
-      
-      if(k.erSpilletVundet()){
-          System.out.println("Tillykke du har vundet!!!");
-      }else if(k.erSpilletTabt()){
-          System.out.println("BUUU du har tabt!!!");
-      }
-      k.nulstil();
-      
-  
-  }
+        URL url = new URL("http://javabog.dk:9901/brugeradmin?wsdl");
+        QName qname = new QName("http://soap.transport.brugerautorisation/", "BrugeradminImplService");
+        Service service = Service.create(url, qname);
+        Brugeradmin auth = service.getPort(Brugeradmin.class);
+
+        Scanner scanner = new Scanner(System.in);
+        String brugerNavn;
+        String password;
+
+        System.out.println("//**\\\\**//**\\\\**//**\\\\**//**\\\\**//**\\\\**//**\\\\**//**\\\\");
+        System.out.println("//                                                  \\\\");
+        System.out.println("//    Velkommen til det fantastiske galgelej spil   \\\\");
+        System.out.println("//                                                  \\\\");
+        System.out.println("//              Intast bruger navn                  \\\\");
+        System.out.println("//                                                  \\\\");
+        System.out.println("//**\\\\**//**\\\\**//**\\\\**//**\\\\**//**\\\\**//**\\\\**//**\\\\");
+
+        brugerNavn = scanner.nextLine();
+
+        System.out.println("//**\\\\**//**\\\\**//**\\\\**//**\\\\**//**\\\\**//**\\\\**//**\\\\");
+        System.out.println("//                                                  \\\\");
+        System.out.println("//    Velkommen til det fantastiske galgelej spil   \\\\");
+        System.out.println("//                                                  \\\\");
+        System.out.println("//              Intast password                     \\\\");
+        System.out.println("//                                                  \\\\");
+        System.out.println("//**\\\\**//**\\\\**//**\\\\**//**\\\\**//**\\\\**//**\\\\**//**\\\\");
+
+        password = scanner.nextLine();
+
+        if (auth.hentBruger(brugerNavn, password) != null) {
+            k.nulstil();
+
+            try {
+                k.hentOrdFraDr();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            k.logStatus();
+
+            System.out.println("Get ordet!!!");
+
+            while (!k.erSpilletSlut()) {
+                System.out.println("Du har gættet forkert " + k.getAntalForkerteBogstaver() + " gange.");
+                System.out.println("Hvis du gætter forkert 7 gange har du tabt :)");
+                System.out.println("");
+                System.out.println("Gæt på et bogstav!!!");
+                System.out.println(k.getSynligtOrd());
+                System.out.println("");
+                System.out.println("Du har gættet på fælgende bogstaver " + k.getBrugteBogstaver());
+
+                k.gætBogstav(scanner.nextLine().toLowerCase());
+                k.logStatus();
+
+            }
+
+            if (k.erSpilletVundet()) {
+                System.out.println("Tillykke du har vundet!!!");
+            } else if (k.erSpilletTabt()) {
+                System.out.println("BUUU du har tabt!!!");
+            }
+            k.nulstil();
+        }else{
+            System.out.println("Brugeren findes ikke!!!!");
+        }
+
+    }
+
 }
